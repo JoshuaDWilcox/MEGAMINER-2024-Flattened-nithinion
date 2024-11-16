08be1ef88f60df1e1d3d9163938cb95088c0260e
xoc <jdwilcox32@gmail.com>
2024-11-15 18:32:55 -0600

    team name changed

--- Joueur.py/games/magomachy/ai.py
+++ Joueur.py/games/magomachy/ai.py
@@ -30,7 +30,7 @@ class AI(BaseAI):
             str: The name of your Player.
         """
         # <<-- Creer-Merge: get-name -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-        return "Magomachy Python Player" # REPLACE THIS WITH YOUR TEAM NAME
+        return "Team Nithinion" # REPLACE THIS WITH YOUR TEAM NAME
         # <<-- /Creer-Merge: get-name -->>
 
     def start(self) -> None:
